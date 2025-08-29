df9aa674f8a947e31bf0869a658dc17cd4e3be8e
bhargav8985 <142100753+bhargav8985@users.noreply.github.com>
2025-08-29 11:24:38 +0530

    added vercel config2

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
-// Your details
 const USER_ID = "endela_bhargav_reddy_15112004";
 const EMAIL = "bhargavreddy8985@gmail.com";
 const ROLL_NUMBER = "22BCE9741";
-
-// Helpers
 function isNumberString(str) {
   return /^-?\d+$/.test(str);
 }
-
 function isAlphabetString(str) {
   return /^[a-zA-Z]+$/.test(str);
 }
-
 function alternateCaps(str) {
   let result = '';
   let upper = true;
@@ -26,12 +20,9 @@ function alternateCaps(str) {
   }
   return result;
 }
-
-// Route
 app.post('/bfhl', (req, res) => {
   try {
     const data = req.body.data;
-
     if (!Array.isArray(data)) {
       return res.status(200).json({
         is_success: false,
@@ -44,17 +35,14 @@ app.post('/bfhl', (req, res) => {
     let alphabets = [];
     let special_characters = [];
     let sum = 0;
-
-    // Collect all alphabets concatenated for concat_string
     let allAlphaConcat = '';
 
     for (let item of data) {
       if (typeof item !== 'string') {
-        item = String(item); // normalize
+        item = String(item);
       }
 
       if (isNumberString(item)) {
-        // Number string
         let num = parseInt(item, 10);
         sum += num;
         if (num % 2 === 0) {
@@ -63,11 +51,9 @@ app.post('/bfhl', (req, res) => {
           odd_numbers.push(item);
         }
       } else if (isAlphabetString(item)) {
-        // Pure alphabets (words allowed, like "ABcD")
         alphabets.push(item.toUpperCase());
         allAlphaConcat += item;
       } else {
-        // Special characters (split into individual chars)
         for (let ch of item) {
           if (!isAlphabetString(ch) && !isNumberString(ch)) {
             special_characters.push(ch);
@@ -75,8 +61,7 @@ app.post('/bfhl', (req, res) => {
         }
       }
     }
-
-    // Reverse allAlphaConcat and apply alternating caps
+    
     let reversedAlpha = allAlphaConcat.split('').reverse().join('');
     let concat_string = alternateCaps(reversedAlpha);
 
